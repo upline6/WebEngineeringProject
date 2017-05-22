80a0bbb50883669c7840859a2cb5fc26152573c6
gengar <gengar@gengar-test2>
2017-05-22 16:16:20 +0200

    Test #1 simple changes

--- WebEngineering/src/testpackage/Testclass.java
+++ WebEngineering/src/testpackage/Testclass.java
@@ -6,6 +6,7 @@ public class Testclass {
 	System.out.println("Test");
 	System.out.println("TEST2");
 	
+	System.out.println(TestclassNo2.dublicate(4));
     }
 
 }
---
+++ WebEngineering/src/testpackage/TestclassNo2.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+public class TestclassNo2 {
+    
+    
+    /**
+     * Heftige Dokumentation am Start
+     * @param zahl simple int as input
+     * @return int double the size of the input
+     */
+    public static int dublicate(int zahl) {
+	return zahl*2;
+    }
+
+}
